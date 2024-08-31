a49db569441b70c91fe553996ef4fa41c9b66670
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-08-31 14:32:38 +0300

    implement delete event button
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/routes/event.$eventId.tsx
+++ app/routes/event.$eventId.tsx
@@ -1,17 +1,37 @@
+import { getAuth } from '@clerk/remix/ssr.server'
 import {
+  ActionFunctionArgs,
   json,
+  redirect,
   type LoaderFunctionArgs,
   type MetaFunction,
 } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import { Form, Link, useLoaderData } from '@remix-run/react'
 import { getUserId } from '~/services/auth.server'
-import { getEvent } from '~/services/events/events.server'
+import { deleteEvent, getEvent } from '~/services/events/events.server'
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'New Remix App' },
     { name: 'description', content: 'Welcome to Remix!' },
   ]
+}
+
+export async function action(args: ActionFunctionArgs) {
+  const userId = await getUserId(args)
+  console.log(`🚀 ~ action ~ userId:`, userId)
+  const body = await args.request.formData()
+  const eventId = body.get('eventId') as string
+  console.log(`🚀 ~ action ~ eventId:`, eventId)
+  if (eventId && userId) {
+    try {
+      await deleteEvent({ id: eventId, userId })
+      return redirect(`/`)
+    } catch (error) {
+      throw new Response('Server error', { status: 500 })
+    }
+  }
+  throw new Response('Bad request', { status: 400 })
 }
 
 export async function loader(args: LoaderFunctionArgs) {
@@ -40,12 +60,35 @@ export default function EventPage() {
       </h1>
       <div className="prose whitespace-pre-line">
         <div className="float-right not-prose p-1 pr-0 text-center">
-          <img
-            alt={event.title}
-            className="h-[128px]"
-            src={`/event-qr/${event.id}.svg`}
-          />
-          <Link to={`/events/upsert/${event.id}`}>{'Update event'}</Link>
+          <div className="flex flex-col gap-2">
+            <img
+              alt={event.title}
+              className="h-[128px]"
+              src={`/event-qr/${event.id}.svg`}
+            />
+            <Link
+              className="btn btn-sm btn-secondary"
+              to={`/events/upsert/${event.id}`}
+            >
+              {'Update event'}
+            </Link>
+            <Form
+              method="post"
+              onSubmit={event => {
+                const response = confirm(
+                  'Please confirm you want to delete this record.',
+                )
+                if (!response) {
+                  event.preventDefault()
+                }
+              }}
+            >
+              <input type="hidden" name="eventId" value={event.id} />
+              <button className="btn btn-sm btn-error w-full" type="submit">
+                Delete event
+              </button>
+            </Form>
+          </div>
         </div>
         {event.body}
       </div>
