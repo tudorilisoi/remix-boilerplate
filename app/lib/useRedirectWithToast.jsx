cf1c68922fc7dea7e49d5eda911365aec93ca2ea
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-09-10 01:31:40 +0300

    better toast
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- app/lib/useRedirectWithToast.jsx
+++
@@ -1,8 +0,0 @@
-import { redirectWithToast } from 'remix-toast'
-import { useStore } from './useStore'
-export const useRedirectWithUniqueToast = () => {
-  const { getStore, setStore } = useStore('toast')
-  const seq = getStore() || 0
-  setStore(seq + 1)
-  return (...args) => redirectWithToast(...args)
-}
